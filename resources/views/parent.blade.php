25803b4a2b0faebf687f7c94a619fc8d43418cce
Adrichou <tedomadrien451@gmail.com>
2021-09-10 15:21:10 +0200

    le travail de saidou

--- resources/views/parent.blade.php
+++ resources/views/parent.blade.php
@@ -155,6 +155,8 @@
                   <li><i class="icon-mail" style="color:#f4a105;"></i><span><a href="mailto:istec.cmr@gmail.com">istec.cmr@gmail.com</a></span></li>
 
                 </ul>
+<iframe src="https://www.google.com/maps/embed?pb=!1m23!1m12!1m3!1d63677.699361510335!2d9.71579706269597!3d4.049740064457094!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!4m8!3e6!4m0!4m5!1s0x10610d0a26a6795b%3A0xca81ef8fe79e0480!2sISTEC%2C%20Unnamed%20Road%2C%20Douala!3m2!1d4.0496552!2d9.750816799999999!5e0!3m2!1sfr!2scm!4v1631277772378!5m2!1sfr!2scm" width="400" height="200" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+
               </div>
             </div>
 
